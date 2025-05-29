a0a1fa31be3b92e6c80b2075ffd6f9f80bba1854
Rizki Ramadhan <rizki.ramadhan01@ui.ac.id>
2025-05-29 18:29:35 +0700

    change info to mine

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,26 +1,33 @@
-import {
-    Card,
-    CardHeader,
-    CardTitle,
-    CardContent
-} from "@/components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 // TODO: Update skills
-const skills = ["TypeScript", "JavaScript", "Python", "Java", "C#", "C++", "C", "SQL", "HTML", "CSS", "TailwindCSS", "NextJS", "ReactJS", "NodeJS", "ExpressJS", "MongoDB", "PostgreSQL", "Git", "Docker", "Kubernetes", "Linux", "Windows", "MacOS"]
+const skills = [
+  "Dart",
+  "Flutter",
+  "Kotlin",
+  "Java",
+  "Jetpack Compose",
+  "Firebase",
+  "Android Studio",
+  "Android",
+  "IOS",
+];
 
 export const Skills = () => {
-    return (
-        <Card className="mt-6">
-            <CardHeader>
-                <CardTitle>Skills</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <div className="flex flex-wrap gap-2">
-                    {skills.map((s, i) => (
-                        <Badge key={i} variant="secondary">{s}</Badge>
-                    ))}
-                </div>
-            </CardContent>
-        </Card>
-    )
-}
+  return (
+    <Card className="mt-6">
+      <CardHeader>
+        <CardTitle>Skills</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-wrap gap-2">
+          {skills.map((s, i) => (
+            <Badge key={i} variant="secondary">
+              {s}
+            </Badge>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
